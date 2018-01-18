4f31f50615528a8e329f1f91daaba49b28b84121
brilliant-ember <fire.senfony@gmail.com>
2018-01-18 18:56:59 -0400

    backbone functions

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+
+void setup() {
+	gameOver = false;
+}
+
+void Draw(){}
+
+void Input(){}
+
+void Logic() {
+
+}
+
+int main() {
+	return 0;
+}
